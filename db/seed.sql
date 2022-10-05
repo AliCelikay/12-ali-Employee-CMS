977cf495b7ef238fb0e1b188dc5bd470434a891c
AliCelikay <111723837+AliCelikay@users.noreply.github.com>
2022-10-04 19:22:51 -0500

    Added the viewAllDepartments function, not working

--- db/seed.sql
+++ db/seed.sql
@@ -4,22 +4,22 @@ VALUES ("Engineering"),
        ("Legal"),
        ("Sales");
 
-INSERT INTO employeeTable (role_title, role_salary)
-VALUES ("Sales Lead"),
-       ("Salesperson"),
-       ("Lead Engineer"),
-       ("Software Engineer")
-       ("Account Manager"),
-       ("Accountant"),
-       ("Legal Team Lead"),
-       ("Lawer");
+INSERT INTO rolesTable (role_title, role_salary)
+VALUES ("Sales Lead", 100000),
+       ("Salesperson", 100000),
+       ("Lead Engineer", 100000),
+       ("Software Engineer", 100000),
+       ("Account Manager", 100000),
+       ("Accountant", 100000),
+       ("Legal Team Lead", 100000),
+       ("Lawer", 100000);
 
--- INSERT INTO employeeTable (first_name, last_name, role_id, manager_id)
--- VALUES ("Naruto", "Uzumaki", "Sales Lead",),
---        ("Salesperson"),
---        ("Lead Engineer"),
---        ("Software Engineer")
---        ("Account Manager"),
---        ("Accountant"),
---        ("Legal Team Lead"),
---        ("Lawer");
+-- INSERT INTO employeeTable (first_name, last_name)
+-- VALUES ("Naruto", "Uzumaki", "Sales Lead");
+--     --    ("Salesperson"),
+--     --    ("Lead Engineer"),
+--     --    ("Software Engineer")
+--     --    ("Account Manager"),
+--     --    ("Accountant"),
+--     --    ("Legal Team Lead"),
+--     --    ("Lawer");
--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2'); 
 const cTable = require('console.table');
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL Username
+      user: 'root',
+      // TODO: Add MySQL Password
+      password: 'rootroot',
+      database: 'employeeCMS_db'
+    },
+    console.log(`Connected to the employeeCMS_db database.`)
+);
+
 //init definition
 const init = () => {
     inquirer
@@ -16,32 +29,49 @@ const init = () => {
         },
     ])
     .promise((ans) => {
+        console.log(ans);
         switch(ans.firstAction)
         {
             case 'View all departments':
+                viewAllDepartments();
             break;
             
             case 'View all roles':
+                viewAllDepartments();
             break;
 
             case 'View all employees':
+                viewAllDepartments();
             break;
 
             case 'Add a department':
+                viewAllDepartments();
             break;
 
             case 'Add a role':
+                viewAllDepartments();
             break;
 
             case 'Add an employee':
+                viewAllDepartments();
             break;
 
             case 'Update an employee role':
+                viewAllDepartments();
             break;
+
+            default:
+                break;
 
         }
     })
 }
+
+const viewAllDepartments = () => {
+    db.query('SELECT * FROM departmentTable', function (err, results) {
+        console.log(results);
+    }
+)}
 
 
 //Calling Initial start of application
